20631dee78c1ab2bbcda4a470f2b39ce4686e69c
Thomas Strothjohann <thomas@strothjohann.net>
2015-03-03 14:18:19 +0000

    Finished FizzBuzz JS.

--- spec/FizzBuzzSpec.js
+++ spec/FizzBuzzSpec.js
@@ -12,6 +12,10 @@ describe('FizzBuzz', function() {
       expect(fizzbuzz.isDivisibleByFive(5)).toBe(true);
     });
 
+    it('by fifteen', function(){
+      expect(fizzbuzz.isDivisibleByFifteen(15)).toBe(true);
+    });
+
   });
 
   describe('It knows when a number is not divisible', function(){
@@ -24,6 +28,31 @@ describe('FizzBuzz', function() {
       expect(fizzbuzz.isDivisibleByFive(1)).toBe(false);
     });
 
+    it('by fifteen', function(){
+      expect(fizzbuzz.isDivisibleByFifteen(1)).toBe(false);
+    });
+
+  });
+
+
+  describe('it should return keyword if divisible', function(){
+    it('by three', function(){
+      expect(fizzbuzz.gameResponse(3)).toEqual("fizz");
+    });
+
+    it('by five', function(){
+      expect(fizzbuzz.gameResponse(5)).toEqual("buzz");
+    });
+
+    it('by fifteen', function(){
+      expect(fizzbuzz.gameResponse(15)).toEqual("fizzbuzz");
+    });
+  });
+
+  describe('it should return the number when not divisible by three or five', function(){
+    it('not by 3 or 5', function(){
+      expect(fizzbuzz.gameResponse(13)).toEqual(13);
+    });
   });
 
 });
--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,12 +1,28 @@
 var FizzBuzz = function(){
 
   FizzBuzz.prototype.isDivisibleByThree = function(number){
-    return number % 3 === 0;
+    return number % 3 === 0
   };
 
   FizzBuzz.prototype.isDivisibleByFive = function(number){
     return number % 5 === 0;
   };
 
+  FizzBuzz.prototype.isDivisibleByFifteen = function(number){
+    return number % 15 === 0;
+  };
 
+  FizzBuzz.prototype.gameResponse = function(number){
+    if (this.isDivisibleByFifteen(number)){
+      return "fizzbuzz";
+    }
+    else if (this.isDivisibleByThree(number)){
+      return "fizz";
+    }
+    else if (this.isDivisibleByFive(number)){
+      return "buzz";
+    }
+    else 
+      return number;
+  };
 };
